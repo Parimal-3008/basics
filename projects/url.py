a7f81c1e439624920a2372048619df6fad776040
Parimal-3008 <parimalyeole30@gmail.com>
2021-09-21 20:44:47 +0530

    django style of using variables in anchor tags

--- projects/url.py
+++ projects/url.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     
-    path('home/<int:id>',views.home, name='home'),
+    path('home/<int:id>',views.home, name='home_variable'),
     path('',views.about, name='about'),
 ]
